0a908f1257fc646811ba9aa4149911262d3c9975
gabriel <gabriel--fiss@hotmail.com>
2026-08-24 22:52:55 -0300

    feat: mock endpoint Ninja

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/controller/MissoesController.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjas.Missoes.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("missoes") // mapear api
+public class MissoesController {
+
+    //@PostMapping("/criar");
+
+}
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/controller/NinjaController.java
@@ -1,6 +1,7 @@
 package dev.java10x.CadastroDeNinjas.Ninjas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,21 @@ public class NinjaController {
     @GetMapping("/")
     public String boasVindas(){
         return "Bem vindo!";
+    }
+
+    @PostMapping("/adicionar")
+    public String criarNinja(){
+        return "Ninja cadastrado";
+    }
+
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas(){
+        return "Mostrar todos ninja";
+    }
+
+    @GetMapping("/todos/id")
+    public String mostrarTodosNinjasPorId(){
+        return "Mostrar ninja por id";
     }
 
  // TODO:    @GetMapping("/ID DO NINJA/missoes") listar todas missoes dele
