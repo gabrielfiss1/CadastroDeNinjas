82d4988b87e00b763df47bf1c9c2b17a87ab8c39
gabriel <gabriel--fiss@hotmail.com>
2026-08-31 22:25:42 -0300

    feat: adiciona lógica na service criarNinja e refactor nas migrations

---
+++ src/main/resources/db/migrations/V1__Create_TB_MISSOES.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tb_missoes (
+                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            nome VARCHAR(255),
+                            dificuldade_missao VARCHAR(50)
+);
